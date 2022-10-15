85221a4aa860bb65e5f25ec3a317e1ab44e71d73
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-15 18:21:47 +0530

    Update GlobalExceptionHandler.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/GlobalExceptionHandler.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/GlobalExceptionHandler.java
@@ -1,5 +1,38 @@
 package com.cg.onlineflatrental.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus; 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(value = FlatBookingNotFoundException.class)
+	public ResponseEntity<String> handleFlatBookingNotFoundException(FlatBookingNotFoundException fbnf)
+	{
+		return new ResponseEntity<String>("Flat booking not found. Please try again.",HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = FlatNotFoundException.class)
+	public ResponseEntity<String> handleFlatNotFoundException(FlatNotFoundException fnf){
+		return new ResponseEntity<String>("Flat not found. Please try again.",HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = LandlordNotFoundException.class)
+	public ResponseEntity<String> handleLandlordNotFoundException(LandlordNotFoundException lnf){
+		return new ResponseEntity<String>("Landlord Not found. Please try again.", HttpStatus.NOT_FOUND);	
+	}
+	
+	@ExceptionHandler(value = TenantNotFoundException.class)
+	public ResponseEntity<String> handleTenantNotFoundException(TenantNotFoundException tnf){
+		return new ResponseEntity<String>("Tenant not found. Please try again.", HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = UserNotFoundException.class)
+	public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException unf)
+	{
+		return new ResponseEntity<String>("User not found. Please try again.",HttpStatus.NOT_FOUND);
+	}
+	
 }
