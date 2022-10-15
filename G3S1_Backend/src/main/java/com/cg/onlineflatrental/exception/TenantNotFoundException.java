5628b365e53d43e0be81c3b62372a6e6dfe3aecd
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-15 13:45:47 +0530

    Update TenantNotFoundException.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/TenantNotFoundException.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/TenantNotFoundException.java
@@ -1,5 +1,14 @@
 package com.cg.onlineflatrental.exception;
 
-public class TenantNotFoundException {
-
+public class TenantNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public TenantNotFoundException() {
+		super();
+	}
+	public TenantNotFoundException(String msg) {
+		super(msg);
+	}
 }
