5f5ba85e4e6c43a70e996660e1df1cb57a635d49
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-15 17:39:39 +0530

    Update ValidationException.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/ValidationException.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/ValidationException.java
@@ -6,6 +6,11 @@ public class ValidationException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public ValidationException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	public ValidationException(String msg) {
 		super(msg);
