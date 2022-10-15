1cdb2d68eaf42be3f40d3d1e45f42d4e433ed74b
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-15 17:38:12 +0530

    Update FlatNotFoundException.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/FlatNotFoundException.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/FlatNotFoundException.java
@@ -5,6 +5,10 @@ public class FlatNotFoundException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public FlatNotFoundException() {
+		super();
+	}
 
 	public FlatNotFoundException(String msg) {
 		super(msg);
