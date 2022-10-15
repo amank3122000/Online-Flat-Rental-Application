26ec2444107346b7004a6e29edaf6ec883f91ba2
Aswin U Nair <aswin.nair923@gmail.com>
2022-10-15 15:15:37 +0530

    User and FlatBooking DTO
    DTO

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/FlatBookingDTO.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/FlatBookingDTO.java
@@ -1,1 +1,52 @@
+package com.cg.onlineflatrental.DTO;
+
+import java.sql.Date;
+
+import com.cg.onlineflatrental.entity.Flat;
+import com.cg.onlineflatrental.entity.Tenant;
+
+public class FlatBookingDTO {
+	private Integer bookingNo;
+	private Flat flat;
+	private Tenant tenantId;
+	private Date bookingFromDate;
+	private Date bookingToDate;
+	
+	public Integer getBookingNo() {
+		return bookingNo;
+	}
+	public void setBookingNo(Integer bookingNo) {
+		this.bookingNo = bookingNo;
+	}
+	public Flat getFlat() {
+		return flat;
+	}
+	public void setFlat(Flat flat) {
+		this.flat = flat;
+	}
+	public Tenant getTenantId() {
+		return tenantId;
+	}
+	public void setTenantId(Tenant tenantId) {
+		this.tenantId = tenantId;
+	}
+	public Date getBookingFromDate() {
+		return bookingFromDate;
+	}
+	public void setBookingFromDate(Date bookingFromDate) {
+		this.bookingFromDate = bookingFromDate;
+	}
+	public Date getBookingToDate() {
+		return bookingToDate;
+	}
+	public void setBookingToDate(Date bookingToDate) {
+		this.bookingToDate = bookingToDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "FlatBookingDTO [bookingNo=" + bookingNo + ", flat=" + flat + ", tenantId=" + tenantId
+				+ ", bookingFromDate=" + bookingFromDate + ", bookingToDate=" + bookingToDate + "]";
+	}
+}
 
--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/UserDTO.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/UserDTO.java
@@ -1,1 +1,39 @@
+package com.cg.onlineflatrental.DTO;
+
+public class UserDTO {
+	int userId;
+	String userName;
+	String password;
+	String userType;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", userName=" + userName + ", password=" + password + ", userType="
+				+ userType + "]";
+	}
+	
+}
 
