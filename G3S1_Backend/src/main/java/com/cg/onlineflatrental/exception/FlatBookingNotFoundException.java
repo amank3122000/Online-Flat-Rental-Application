0a1c084aaba597ce4dd9a6e0651fee90617b992d
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-15 13:38:22 +0530

    Update FlatBookingNotFoundException.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/FlatBookingNotFoundException.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/exception/FlatBookingNotFoundException.java
@@ -1,5 +1,14 @@
 package com.cg.onlineflatrental.exception;
 
-public class FlatBookingNotFoundException {
-
+public class FlatBookingNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public FlatBookingNotFoundException() {
+		super();
+	}
+	public FlatBookingNotFoundException(String msg) {
+		super(msg);
+	}
 }
