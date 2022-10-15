5b4eb79df861ac7030c45ed784a551b0ce9402d0
ElsintJ <115775376+ElsintJ@users.noreply.github.com>
2022-10-15 18:29:57 +0530

    quickfix

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/LandlordDTO.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/DTO/LandlordDTO.java
@@ -1,28 +1,38 @@
-package com.cg.onlineflatrental.dto;
+package com.cg.onlineflatrental.DTO;
 
 import java.util.List;
-
 import com.cg.onlineflatrental.entity.Flat;
 
-
 public class LandlordDTO {
-
 	private int landlordId;
 	private String landlordName;
 	private int landlordAge;
 	private List<Flat> flatList;
-  
-	public LandlordDTO() {}
-	public LandlordDTO( String landlordName, int landlordAge, List<Flat> flatList) {
+	/**
+	 * 
+	 */
+	public LandlordDTO() {
 		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param landlordId
+	 * @param landlordName
+	 * @param landlordAge
+	 * @param flatList
+	 */
+	public LandlordDTO(int landlordId, String landlordName, int landlordAge, List<Flat> flatList) {
+		super();
+		this.landlordId = landlordId;
 		this.landlordName = landlordName;
 		this.landlordAge = landlordAge;
 		this.flatList = flatList;
 	}
-	public Integer getLandlordId() {
+	
+	public int getLandlordId() {
 		return landlordId;
 	}
-	public void setLandlordId(Integer landlordId) {
+	public void setLandlordId(int landlordId) {
 		this.landlordId = landlordId;
 	}
 	public String getLandlordName() {
@@ -37,15 +47,15 @@ public class LandlordDTO {
 	public void setLandlordAge(int landlordAge) {
 		this.landlordAge = landlordAge;
 	}
-	public List<Flat> getFlatList() { 
-		  return flatList; 
-	} 
-	public void setFlatList(List<Flat> flatList) { 
-		  this.flatList = flatList;
+	public List<Flat> getFlatList() {
+		return flatList;
+	}
+	public void setFlatList(List<Flat> flatList) {
+		this.flatList = flatList;
 	}
 	@Override
 	public String toString() {
-		return "LandlordDTO [landlordId=" + landlordId + ", landlordName=" + landlordName + ", landlordAge=" + landlordAge
-				+ ", flatList=" + flatList + "]";
+		return "LandlordDTO [landlordId=" + landlordId + ", landlordName=" + landlordName + ", landlordAge="
+				+ landlordAge + ", flatList=" + flatList + "]";
 	}
 }
