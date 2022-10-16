3ebb36b745255619e956312dd133a7389d4665f3
ElsintJ <115775376+ElsintJ@users.noreply.github.com>
2022-10-16 20:55:07 +0530

    service

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/ILandlordService.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/ILandlordService.java
@@ -1,5 +1,18 @@
 package com.cg.onlineflatrental.service;
 
+import java.util.List;
+import com.cg.onlineflatrental.entity.Landlord;
+import com.cg.onlineflatrental.exception.LandlordNotFoundException;
+
 public interface ILandlordService {
+	public Landlord addLandlord(Landlord landlord);
+
+	public Landlord updateLandlord(Integer landlordId, Landlord landlord) throws LandlordNotFoundException;
+
+	public void deleteLandlord(Integer landlordId) throws LandlordNotFoundException;
+
+	public Landlord viewLandlord(Integer landlordId) throws LandlordNotFoundException;
+
+	public List<Landlord> viewAllLandlord();
 
 }
--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/LandlordService.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/LandlordService.java
@@ -1,5 +1,57 @@
 package com.cg.onlineflatrental.service;
 
-public class LandlordService {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.cg.onlineflatrental.entity.Landlord;
+import com.cg.onlineflatrental.exception.LandlordNotFoundException;
+import com.cg.onlineflatrental.repository.ILandlordRepository;
+
+@Service
+public class LandlordService implements ILandlordService {
+
+	@Autowired
+	private ILandlordRepository landlordRepository;
+
+	@Override
+	public Landlord addLandlord(Landlord landlord) {
+		landlordRepository.save(landlord);
+		return landlord;
+	}
+
+	@Override
+	public void deleteLandlord(Integer landlordId) throws LandlordNotFoundException {
+		if (!landlordRepository.existsById(landlordId)) {
+			throw new LandlordNotFoundException();
+		}
+		landlordRepository.deleteById(landlordId);
+	}
+
+	@Override
+	public Landlord viewLandlord(Integer landlordId) throws LandlordNotFoundException {
+
+		if (!landlordRepository.existsById(landlordId)) {
+			throw new LandlordNotFoundException();
+		}
+		return landlordRepository.findById(landlordId).get();
+	}
+
+	@Override
+	public List<Landlord> viewAllLandlord() {
+		return (List<Landlord>) landlordRepository.findAll();
+	}
+
+	@Override
+	public Landlord updateLandlord(Integer landlordId, Landlord landlord) throws LandlordNotFoundException {
+		if (!landlordRepository.existsById(landlordId)) {
+			throw new LandlordNotFoundException();
+		}
+		Landlord l = landlordRepository.findById(landlordId).get();
+		l.setLandlordName(landlord.getLandlordName());
+		l.setLandlordAge(landlord.getLandlordAge());
+		l.setFlatList(landlord.getFlatList());
+
+		return landlordRepository.save(l);
+	}
 }
