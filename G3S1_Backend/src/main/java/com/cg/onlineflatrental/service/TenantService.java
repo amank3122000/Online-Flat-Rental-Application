83b54d1903e3a4f82fc8a8beaf7404c7c89256b2
Amar Khedkar <smartynew0@gmail.com>
2022-10-16 14:00:14 +0530

    Service

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/TenantService.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/service/TenantService.java
@@ -1,5 +1,17 @@
 package com.cg.onlineflatrental.service;
 
-public class TenantService {
-//hdafuds
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.Service;
+
+import com.cg.onlineflatrental.entity.Tenant;
+import com.cg.onlineflatrental.repository.ITenantRepository;
+import com.cg.onlineflatrental.exception.TenantNotFoundException;
+import java.util.Optional;
+public class TenantService implements ITenantService {
+	private static final Logger logger = LogManager.getLogger(TenantService.class);
+	@Autowired
+	private ITenantRepository tenantRepository;
+	
 }
