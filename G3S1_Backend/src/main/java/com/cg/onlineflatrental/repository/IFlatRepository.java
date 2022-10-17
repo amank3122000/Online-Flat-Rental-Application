dcd6d06cd0219b013f83afa3a3e0fb80826f009d
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-10-17 10:35:25 +0530

    Update IFlatRepository.java

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/repository/IFlatRepository.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/repository/IFlatRepository.java
@@ -1,5 +1,11 @@
 package com.cg.onlineflatrental.repository;
 
-public interface IFlatRepository {
+import java.util.List;  
+import org.springframework.data.jpa.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.cg.onlineflatrental.entity.Flat;
 
+@Repository
+public interface IFlatRepository extends CrudRepository<Flat,Integer>{
+	List<Flat> findByCostAndAvailability(float cost, String availability);
 }
