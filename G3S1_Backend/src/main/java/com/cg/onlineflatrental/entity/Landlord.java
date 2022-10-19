6089c9ebc25e53edc0d434c450a3607aeefea924
ElsintJ <115775376+ElsintJ@users.noreply.github.com>
2022-10-19 11:58:22 +0530

    .

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/entity/Landlord.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/entity/Landlord.java
@@ -2,8 +2,6 @@ package com.cg.onlineflatrental.entity;
 import java.util.List; 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
@@ -15,7 +13,6 @@ import javax.validation.constraints.NotNull;
 @Entity
 public class Landlord {
 	@Id
-	//@GeneratedValue(strategy = GenerationType.AUTO)
 	private int landlordId;
 	@NotNull(message = "Landlord name is mandatory")
 	private String landlordName;
