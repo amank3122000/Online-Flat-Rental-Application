744364ee21a18f53ff362fdfed43fcbb7360b914
Aswin U Nair <aswin.nair923@gmail.com>
2022-11-03 19:57:32 +0530

    .

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/entity/Landlord.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/entity/Landlord.java
@@ -2,6 +2,8 @@ package com.cg.onlineflatrental.entity;
 import java.util.List; 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
