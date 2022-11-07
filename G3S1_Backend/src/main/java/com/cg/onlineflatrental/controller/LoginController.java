36b9fdb938e07792cf31608f943874eaea82c806
Amar Khedkar <smartynew0@gmail.com>
2022-11-07 13:31:13 +0530

    .

--- G3S1_Backend/src/main/java/com/cg/onlineflatrental/controller/LoginController.java
+++ G3S1_Backend/src/main/java/com/cg/onlineflatrental/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.cg.onlineflatrental.controller;
 
+import javax.security.auth.login.LoginException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.onlineflatrental.entity.User;
+import com.cg.onlineflatrental.exception.UserNotFoundException;
+import com.cg.onlineflatrental.service.ILoginService;
+import com.cg.onlineflatrental.service.UserService;
+
+import io.swagger.annotations.Api;
 
 
 
@@ -27,10 +36,10 @@ public class LoginController {
 
     @GetMapping(value = "/authenticateUser/{username}/{password}/{usertype}")
 
-	public ResponseEntity<User> login(@PathVariable String username, @PathVariable String password, @PathVariable String usertype) {
+	public ResponseEntity<User> login(@PathVariable String username, @PathVariable String password, @PathVariable String usertype) throws LoginException, UserNotFoundException {
 		ResponseEntity<User> rentity;
 		if (service.login(username,password,usertype)) {
-			User user = new User(username,password,usertype);
+			User user = new User(null, username,password,usertype);
 			rentity = new ResponseEntity<User>(user, HttpStatus.ACCEPTED)  ;
 		} else {
 			rentity = new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
