b159b3505a14b44e839370c92544fc5226f7732f
amank3122000 <92937257+amank3122000@users.noreply.github.com>
2022-11-08 14:16:03 +0530

    Update RemoveFlat.js

---
+++ g3s2_frontend/src/components/Admin-page/Flat/RemoveFlat.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function RemoveFlat() {
+
+  return (
+      <React.Fragment>
+        <h1>Remove flat</h1>
+
+    
+     </React.Fragment>
+    );
+}
+
+export default RemoveFlat;
