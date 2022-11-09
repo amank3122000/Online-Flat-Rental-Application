e4fb136174977a815a285f235aadee2c70f26d02
subhash3108 <115794805+subhash3108@users.noreply.github.com>
2022-11-10 00:18:10 +0530

    booking

--- g3s2_frontend/src/components/Admin-page/Booking/AddBooking.js
+++ g3s2_frontend/src/components/Admin-page/Booking/AddBooking.js
@@ -1,9 +1,29 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function AddBooking() {
+
   return (
-    <div>AddBooking</div>
-  )
+      <React.Fragment>
+       <form class="c2">
+    <h1 class="form-text">Add Booking</h1>
+    <br/><br/>
+    <input name="flatid" type="number" placeholder="Flat ID" class="col-md-4 address-tags"/>
+    <input name="tenantid" type="number" placeholder="Tenant ID" class="col-md-4 address-tags"/>
+    <br/>
+    <label for="" className="lable-tag col-md-3">From</label>
+    <input type="date" value="2017-06-01" class="col-md-4 address-tags"/> 
+    <label for="" className="lable-tag col-md-3">To</label>
+    <input type="date" value="2017-07-01" class="col-md-4 address-tags"/>
+
+    <br/>
+    <button class="btn">Book Flat</button>
+ </form>
+    
+
+     </React.Fragment>
+    );
 }
 
-export default AddBooking
+export default AddBooking;
