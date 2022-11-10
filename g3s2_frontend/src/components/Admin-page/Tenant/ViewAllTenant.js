0d87f02735779d515137839178e2c873210241a5
Radhika Agarwal <agarwal99radhika@gmail.com>
2022-11-10 14:21:52 +0530

    .

--- g3s2_frontend/src/components/Admin-page/Tenant/ViewAllTenant.js
+++ g3s2_frontend/src/components/Admin-page/Tenant/ViewAllTenant.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function ViewAllTenant() {
 
     let initialCustomers=[];
-    let [tenents,setTenants]=useState(initialCustomers);
+    let [tenants,setTenants]=useState(initialCustomers);
 
     useEffect(() => {
         const URL = 'http://localhost:8080/tenant/viewAllTenant';
@@ -14,37 +14,37 @@ function ViewAllTenant() {
         .catch((error) => console.log(error));
     },[]);
 
-    function goToAddCustomer(){
-        //props.history.push('/addCustomer');
-    }
+     function goToAddCustomer(){
+    //     //props.history.push('/addCustomer');
+     }
 
 
   return (
       <React.Fragment>
-            <table class="table table-data table-striped table-bordered">
+            <table className="table table-data table-striped table-bordered">
     <thead>
-        <tr class="table-warning">
-          <th class="col-md-2">ID</th>
-          <th class="col-md-3">Tenant Name</th>
-          <th class="col-md-1">Age</th>
-          <th class="col-md-3">Flat List</th>
-          <th class="col-md-3">Actions</th>
+        <tr className="table-warning">
+          <th className="col-md-2">ID</th>
+          <th className="col-md-3">Tenant Name</th>
+          <th className="col-md-1">Age</th>
+          <th className="col-md-3">Flat List</th>
+          <th classclassName="col-md-3">Actions</th>
         </tr>
       </thead>
       <tbody class="table-success ">
       {
-                tenents.map (
+                tenants.map (
                     tenant =>
         <tr>
-          <td class="col-md-2">{tenant.tenantId}</td>
-          <td class="col-md-3">{tenant.tenantName}</td>
-          <td class="col-md-1">{tenant.age}</td>
-          <td class="col-md-3">
-            <button class="btn btn-info logout-btn" type="submit">Flat List</button>
+          <td className="col-md-2">{tenant.tenantId}</td>
+          <td className="col-md-3">{tenant.tenantName}</td>
+          <td className="col-md-1">{tenant.age}</td>
+          <td className="col-md-3">
+            <button className="btn btn-info logout-btn" type="submit">Flat List</button>
           </td>
           <td class="col-md-3">
-            <button class="btn btn-warning logout-btn" type="submit">Update</button>
-            <button class="btn btn-danger logout-btn" type="submit">Delete</button>
+            <button className="btn btn-warning logout-btn" type="submit">Update</button>
+            <button className="btn btn-danger logout-btn" type="submit">Delete</button>
           </td>
         </tr>
       
