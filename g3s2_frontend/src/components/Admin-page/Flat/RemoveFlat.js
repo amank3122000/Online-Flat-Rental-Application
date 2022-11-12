1ea0053ae8e8ae88e351ada5c142af12f9d92d7b
amank3122000 <amank3122000@gmail.com>
2022-11-12 16:00:15 +0530

    .

--- g3s2_frontend/src/components/Admin-page/Flat/RemoveFlat.js
+++ g3s2_frontend/src/components/Admin-page/Flat/RemoveFlat.js
@@ -1,15 +1,81 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+
+// function RemoveFlat() {
+
+//   return (
+//       <React.Fragment>
+//         <h1>Remove flat</h1>
+
+    
+//      </React.Fragment>
+//     );
+// }
+
+// export default RemoveFlat;
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function RemoveFlat() {
 
-  return (
-      <React.Fragment>
-        <h1>Remove flat</h1>
+   const [flatid,setFlatid] = useState(0);
+   const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
 
-    
-     </React.Fragment>
+
+  const handleBtnClick = (e)=>{
+      // console.log(e.target.response)
+      e.preventDefault();
+      setFormErrors(validate(flatid));
+      setIsSubmit(true);
+      axios.delete(`http://localhost:8080/flat/deleteflat/${flatid}`)
+      .then(() => {
+        window.alert("Flat Removed");
+        setFlatid(0);
+      }).catch(error => console.log(error.message));
+  }
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(flatid);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values) {
+        errors.userid = "User id is required!";
+    }
+    return errors;
+  };
+
+   return (
+      <React.Fragment>
+        <nav className="navbar navbar-dark bg-dark justify-content-between fixed-top">
+        <Link className="navbar-brand navbar-brand-margin">Admin Panel</Link>
+        <span className="header-right">Flat Rental Application</span>
+        <form className="form-inline">
+       
+          <button className="btn btn-outline-danger my-2 my-sm-0 logout-btn" type="submit"><a href="/login">Logout</a></button>
+        </form>
+      </nav>
+        <form className="c2 view-form">
+            <h1 className="form-text ">Delete Flat By ID</h1>
+           <br/>
+           <label>Flat Id</label>
+            <input name="flatid" type="number" placeholder="Flat ID*" className="username" 
+             value = {flatid}
+             onChange={e=>setFlatid(e.target.value)}
+            />
+            <br/>
+            <p>{formErrors.userid}</p>
+            <button className="btn" data-testid="button" onClick={handleBtnClick}>Delete Flat</button>
+         </form>
+    </React.Fragment>
     );
 }
 
-export default RemoveFlat;
+export default RemoveFlat; 
